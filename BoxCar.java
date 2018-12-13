0c44f425378c73f99ca8b2d02c479204b8e58a43
BenStoakes <45640287+BenStoakes@users.noreply.github.com>
2018-12-13 14:45:07 -0600

    Update BoxCar.java

--- BoxCar.java
+++ BoxCar.java
@@ -4,16 +4,28 @@
  * @version 1.00 2018/12/11
  */
 
-import java.util.Scanner;
 public class BoxCar
 {
-    public void BoxCar (int dieOne, int dieTwo)
-   {
-    Scanner scan = new Scanner (System.in);	
-    	
-   int boxCars = 0;
+    public void BoxCarMethod(int dieOne,int dieTwo)
+  {
+   int boxCarTrue = 0;
+   int OneSix = 0;
+   int TwoSix = 0;
    	
    	//===================
-   if (dieOne == dieTwo) dieOne = 6; boxCars ++;
+   if(dieOne == 6)
+   {
+   		OneSix = 1;
    }
+   //------------------------
+   if(dieTwo == 6)
+   {
+   		TwoSix = 1;
+   }
+   //-------------------------
+   if(OneSix == TwoSix)
+   {  
+   		boxCarTrue = 1;
+   }
+ }
 }
